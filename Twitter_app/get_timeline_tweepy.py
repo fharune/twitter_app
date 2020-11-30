774029332957c1f73d01bb59836c212aea0bda2b
fharune <66402295+fharune@users.noreply.github.com>
2020-11-30 15:48:31 +0900

    Update get_timeline_tweepy.py

--- Twitter_app/get_timeline_tweepy.py
+++ Twitter_app/get_timeline_tweepy.py
@@ -17,25 +17,27 @@ api = tweepy.API(auth)
 
 class MyStreamListener(tweepy.StreamListener):
     def on_status(self, status):
-        print("------------------------------------")
-        print("tweet id:", status.id)
-        print("screen_name:", status.user.screen_name)
-        print("user name:", status.user.name)
-        print(status.text)
+	if (not status.retweeted) and ("RT @" not in status.text) and ("@" not in status.text):
+		print("------------------------------------")
+		print("tweet id:", status.id)
+		print("screen_name:", status.user.screen_name)
+		print("user name:", status.user.name)
+		print(status.text)
+	
+        stream = tweepy.Stream(auth,MyStreamListener())
+        stream.filter(follow = ['69889591', '2268227281', '733275531500605441', '1059024253532524544', '1257339421440880640']) 
 
 myStreamListener = MyStreamListener()
 myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
 
-if __name__ == '__main__':
-	stream = tweepy.Stream(auth,MyStreamListener())
-	while True :
-			stream.filter(follow = [''])
 
 
 @app.route('/')
 def index():
     header = "Tweet Recommendation"
-    return render_template("index.html")
+    stream = myStream
+    tweets = api.user_timeline(stream, count = 200)
+    return render_template("index.html", header = header, tweets = tweets)   
 
 if __name__ == "__main__":
     app.run(debug=True)
